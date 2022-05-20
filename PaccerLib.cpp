e5b9913e5f2b1bf62b991436689e27bfb0f6325d
Krxwallo <ken.grill@gmx.de>
2022-05-20 20:09:38 +0200

    example library

---
+++ PaccerLib.cpp
@@ -0,0 +1,21 @@
+/*
+ * PaccerLib
+ * =========
+ * 
+ * Library for the Paccer Pinball.
+ * 
+ * Created 2022-05-20
+ * 
+ * https://github.com/PaccerPinball/PaccerLib
+ */
+
+
+#include "PaccerLib.h"
+
+int addTwoInts(int a, int b) {
+  return a + b;
+}
+
+int addTwoDoubles(double a, double b) {
+    return a + b;
+}
---
+++ PaccerLib.h
@@ -0,0 +1,19 @@
+/*
+ * PaccerLib
+ * =========
+ * 
+ * Library for the Paccer Pinball.
+ * 
+ * Created 2022-05-20
+ * 
+ * https://github.com/PaccerPinball/PaccerLib
+ */
+
+
+#ifndef PACCERLIB_H
+#define PACCERLIB_H
+#include <Arduino.h>
+
+int addTwoInts(int a, int b);
+
+#endif
